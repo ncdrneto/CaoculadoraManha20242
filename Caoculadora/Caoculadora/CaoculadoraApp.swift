068b9f2de9b7c1c7876000ab76b2f4692b205a6a
Francisco Soares <fmssn@cin.ufpe.br>
2024-05-07 11:30:52 -0300

    cria projeto da caoculadora no Xcode

---
+++ Caoculadora/Caoculadora/CaoculadoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaoculadoraApp.swift
+//  Caoculadora
+//
+//  Created by Francisco Miranda Soares on 07/05/24.
+//
+
+import SwiftUI
+
+@main
+struct CaoculadoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Caoculadora/Caoculadora/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Caoculadora
+//
+//  Created by Francisco Miranda Soares on 07/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "pawprint")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
