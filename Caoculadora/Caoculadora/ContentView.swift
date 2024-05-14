ea817c828f207e12600a32508631c0568f233abf
Juliano Vaz <julianoctvaz@gmail.com>
2024-05-14 19:46:03 -0300

    Updates new contents of class

--- Caoculadora/Caoculadora/ContentView.swift
+++ Caoculadora/Caoculadora/ContentView.swift
@@ -7,6 +7,8 @@
 
 import SwiftUI
 
+//MARK: - View
+
 struct ContentView: View {
 
     @State var years: Int?
@@ -14,98 +16,113 @@ struct ContentView: View {
     @State var result: Int?
 
     @State var porteSelected = Porte.pequeno
+    @State var failedInput = false
+    let tituloPreencherCampos = "Preencha os campos para cãocular!"
 
     var body: some View {
-        VStack(alignment: .leading, spacing: 20.0) {
-            Text("Qual a idade do seu cão?")
-                .font(.header5)
-            Text("Anos")
-                .font(.body1)
-            TextField(
-                "Quantos anos completos tem seu cão",
-                value: $years,
-                format: .number
-            )
-            Text("Meses")
-                .font(.body1)
-            TextField(
-                "E quantos meses além disso ele tem",
-                value: $months,
-                format: .number
-            )
-            Text("Porte")
-                .font(.body1)
+        NavigationStack {
+            ScrollView {
+                VStack(alignment: .leading, spacing: 20.0) {
+                    Text("Qual a idade do seu cão?")
+                        .font(.header5)
+                    Text("Anos")
+                        .font(.body1)
+                    TextField(
+                        "Quantos anos completos tem seu cão",
+                        value: $years,
+                        format: .number
+                    )
+                    Text("Meses")
+                        .font(.body1)
+                    TextField(
+                        "E quantos meses além disso ele tem",
+                        value: $months,
+                        format: .number
+                    )
+                    Text("Porte")
+                        .font(.body1)
 
-            // aqui vai o segmented control
-            Picker("Portes", selection: $porteSelected) {
-                ForEach(Porte.allCases, id:\.self) { porte in
-                    Text(porte.rawValue)
-                }
-            }
-            .pickerStyle(.segmented)
+                    // aqui vai o segmented control
+                    Picker("Portes", selection: $porteSelected) {
+                        ForEach(Porte.allCases, id:\.self) { porte in
+                            Text(porte.rawValue)
+                        }
+                    }
+                    .pickerStyle(.segmented)
 
-            Divider()
+                    Divider()
 
-            Spacer()
+                    Spacer()
 
-            if let result {
-                Text("Seu cachorro tem, em idade humana...")
-                    .font(.body1)
-                Text("\(result) anos")
-                    .font(.display)
-            } else {
-                Image(ImageResource.clarinha)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(maxHeight: 150)
+                    if let result {
+                        Text("Seu cachorro tem, em idade humana...")
+                            .font(.body1)
+                            .frame(maxWidth: .infinity)
+                        Text("\(result) anos")
+                            .font(.display)
+                            .frame(maxWidth: .infinity)
+                            .contentTransition(.numericText())
+                    } else {
+                        Image(ImageResource.clarinha)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(maxHeight: 150)
+                            .frame(maxWidth: .infinity)
+                            .shadow(radius: 10)
+                    }
+
+                    Spacer()
+
+                    Button("Cãocular", action: processYears)
                     .frame(maxWidth: .infinity)
-                    .shadow(radius: 10)
+                    .frame(height: 50)
+                    .background(.indigo)
+                    .foregroundStyle(.white)
+                    .clipShape(.rect(cornerRadius: 10))
+                    .font(.body1)
+                }
+                .textFieldStyle(.roundedBorder)
+                .keyboardType(.numberPad)
+                .padding()
+                .containerRelativeFrame(.vertical)
+//                .animation(.easeInOut.speed(0.5), value: result)
             }
-
-            Spacer()
-
-            Button("Cãocular", action: processYears)
-            .frame(maxWidth: .infinity)
-            .frame(height: 50)
-            .background(.indigo)
-            .foregroundStyle(.white)
-            .clipShape(.rect(cornerRadius: 10))
-            .font(.body1)
+            .alert(tituloPreencherCampos,
+                   isPresented: $failedInput,
+                   actions: {
+                Button("OK", role: .cancel, action: {})
+            })
+            .navigationTitle("Cãoculadora")
+            .scrollDismissesKeyboard(.immediately)
+            .toolbarBackground(.indigo600, for: .navigationBar)
+            .toolbarBackground(.visible, for: .navigationBar)
+            .toolbarColorScheme(.dark, for: .navigationBar)
         }
-        .textFieldStyle(.roundedBorder)
-        .keyboardType(.numberPad)
-        .padding()
+        .fontDesign(.rounded)
     }
+}
 
+//MARK: - Functions
+
+extension ContentView {
     func processYears() {
         print("cãocular")
         
         guard let years, let months else {
             print("campos não preenchidos")
+            failedInput = true
             return
         }
-
+        
         guard months > 0 || years > 0 else {
             print("pelo menos um campo deve ser maior que zero")
             return
         }
-
-        // o resultado vai ser os anos * multiplicador + a fração do ano em meses * multiplicador
-        // multiplicador:
-        //   * pequeno: 6
-        //   * médio: 7
-        //   * grande: 8
-        let multiplicador: Int
-        switch porteSelected {
-        case .pequeno:
-            multiplicador = 6
-        case .medio:
-            multiplicador = 7
-        case .grande:
-            multiplicador = 8
+        
+        withAnimation(.easeInOut.speed(0.5)) {
+            result = porteSelected.calcularIdade(deAnos: years,
+                                                 eMeses: months)
         }
-
-        result = years * multiplicador + months * multiplicador / 12
     }
 }
 
--- Caoculadora/Caoculadora/Porte.swift
+++ Caoculadora/Caoculadora/Porte.swift
@@ -12,5 +12,30 @@ enum Porte: String, CaseIterable {
     case medio = "Médio"
     case grande = "Grande"
 
-    
+    func calcularIdade(deAnos anosCaninos: Int,
+                      eMeses mesesCaninos: Int) -> Int {
+        // o resultado vai ser os anos * multiplicador + a fração do ano em meses * multiplicador
+        // multiplicador:
+        //   * pequeno: 6
+        //   * médio: 7
+        //   * grande: 8
+        
+        let multiplicador: Int
+        
+        switch self {
+        case .pequeno:
+            multiplicador = 6
+        case .medio:
+            multiplicador = 7
+        case .grande:
+            multiplicador = 8
+        }
+
+        let result = anosCaninos * multiplicador + mesesCaninos * multiplicador / 12
+        
+        return result
+        
+        
+    }
 }
+
