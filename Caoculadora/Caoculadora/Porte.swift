78d88c7dce1c1d203e47e9e4a4a80087511abe71
Newton Neto <ncrneto@gmail.com>
2024-06-17 11:24:14 -0300

    Erase comments

--- Caoculadora/Caoculadora/Porte.swift
+++ Caoculadora/Caoculadora/Porte.swift
@@ -14,11 +14,6 @@ enum Porte: String, CaseIterable {
 
     func calcularIdade(deAnos anosCaninos: Int,
                       eMeses mesesCaninos: Int) -> Int {
-        // o resultado vai ser os anos * multiplicador + a fração do ano em meses * multiplicador
-        // multiplicador:
-        //   * pequeno: 6
-        //   * médio: 7
-        //   * grande: 8
         
         let multiplicador: Int
         
